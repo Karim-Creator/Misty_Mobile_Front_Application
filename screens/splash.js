c913864a2d6a4f0ee484921a7557238d7bc0b1e3
karim <develkarim@gmail.com>
2021-11-20 16:00:36 +0100

    Pushing changes

---
+++ screens/splash.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View,Image } from 'react-native'; 
+import { Spinner } from 'native-base'
+
+
+export default class SplashScreen extends Component {
+
+    render(){
+        return(
+            <View style={{flex: 1,backgroundColor:'#fff',flexDirection:'column',justifyContent:'center'}}>
+                <Image source={require('../assets/Logo/logo.png')} style={{alignSelf:'center',alignItems:'center',resizeMode:'contain'}} />
+                <Spinner color='green' />
+            </View>
+        )
+    }
+}
---
+++ utils/WeatherAPIKey.js
@@ -0,0 +1,2 @@
+//OpenWeatherMap API Key
+export const API_KEY = 'ae49d2146ccceb909390e29a6ee3118a';
